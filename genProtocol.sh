b419d7379e1f4afc36e6fad8c3907892b63201e6
Jason Holloway <miceeatnicerice@hotmail.com>
2017-09-10 18:13:27 +0100

    improved genProtocol to strip package

--- genProtocol.sh
+++ genProtocol.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
-protoVersion=v100
-protoFile=../store/store/src/main/resources/${protoVersion}.proto
+set -x
 
-node_modules/.bin/pbjs -t static-module -w commonjs -o protocol/${protoVersion}.js ${protoFile}
-node_modules/.bin/pbts -o protocol/${protoVersion}.d.ts protocol/${protoVersion}.js
+protoVersion=v100
+protoFile=../store/store/src/main/protobuf/${protoVersion}.proto
+targetDir=./src/protocol
+
+tempFile=temp.proto~
+
+function main {
+    rm -f $tempFile
+
+    cat ${protoFile} \
+        | egrep -v '^package .*$' \
+        > $tempFile
+
+    pbjs -t static-module -r b3 -w es6 -o ${targetDir}/${protoVersion}.js $tempFile
+    rm -f $tempFile
+
+    pbts -o ${targetDir}/${protoVersion}.d.ts ${targetDir}/${protoVersion}.js
+}
+
+function pbjs {
+    node_modules/.bin/pbjs $@
+}
+
+function pbts {
+    node_modules/.bin/pbts $@
+}
+
+main
